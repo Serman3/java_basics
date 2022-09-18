cc0b512d8599c263b36390dba0d3aef3ee2a1735
sergei_kozlov_7 <kozlovsergej266@gmail.com>
2022-09-18 21:22:20 +0300

    initialised variable count, add for

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -3,6 +3,23 @@ public class Main {
         Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
+        container.addCount(5672);
+        System.out.println(container.getCount());
+
+
+        for(int i = 0; i <= 65536; i++){
+            char c = (char) i;
+           // System.out.println(i + " - " + c);
+            if (i == 1025){
+                System.out.println(c);
+            }
+            if (i >= 1040 && i <= 1103){
+                System.out.println(c);
+            }
+            if (i == 1105){
+                System.out.println(c);
+            }
+        }
 
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
